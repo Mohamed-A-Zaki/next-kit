6713696233d889a5ee0194bcab9d2a37646d0d49
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2025-03-26 00:11:11 +0200

    feat: add PasswordInput component with toggle visibility for password field

---
+++ components/ui/password-input.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState } from "react";
+import { Input } from "./input";
+
+export const PasswordInput = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="relative">
+      <Input type={showPassword ? "text" : "password"} />
+      <div className="absolute top-0 right-0 h-full cursor-pointer px-3 py-2 hover:bg-transparent">
+        {showPassword ? (
+          <EyeIcon className="h-4 w-4" onClick={() => setShowPassword(false)} />
+        ) : (
+          <EyeOffIcon
+            className="h-4 w-4"
+            onClick={() => setShowPassword(true)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
