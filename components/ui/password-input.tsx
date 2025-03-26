f4b1b46b6d6c98cf3210bf0632d30f3866a38bb5
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2025-03-26 02:25:07 +0200

    feat: enhance PasswordInput component to accept className and additional props

--- components/ui/password-input.tsx
+++ components/ui/password-input.tsx
@@ -4,12 +4,19 @@ import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { useState } from "react";
 import { Input } from "./input";
 
-export const PasswordInput = () => {
+export const PasswordInput = ({
+  className,
+  ...props
+}: React.ComponentProps<"input">) => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="relative">
-      <Input type={showPassword ? "text" : "password"} />
+      <Input
+        type={showPassword ? "text" : "password"}
+        {...props}
+        className={className}
+      />
       <div className="absolute top-0 right-0 h-full cursor-pointer px-3 py-2 hover:bg-transparent">
         {showPassword ? (
           <EyeIcon className="h-4 w-4" onClick={() => setShowPassword(false)} />
